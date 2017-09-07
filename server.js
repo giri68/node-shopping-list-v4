1da67588b2c2fc4e69569341d7845ef98329ede0
giri68 <giri.pdt@gmail.com>
2017-09-07 09:46:12 -0700

    Added functionality to update receipes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -74,6 +74,31 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
     id: req.params.id,
     name: req.body.name,
     budget: req.body.budget
+  });
+  res.status(204).end();
+});
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'id', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    console.log('here');
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipes item \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
   });
   res.status(204).end();
 });
